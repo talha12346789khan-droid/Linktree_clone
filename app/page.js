008189437ab2583d076ab6ebfd2559836926ca28
BalouchGITHUB <talha12346789khan@gmail.com>
2026-01-11 23:18:43 +0500

    11/1/2026

--- app/page.js
+++ app/page.js
@@ -3,8 +3,18 @@ import Image from "next/image";
 export default function Home() {
   return (
    <main>
-    <section className="bg-green-600 min-h-screen">
+    <section className="bg-[#d2e823] min-h-screen">
+    <div className="hero flex items-center gap-0 py-130 px-10">
+      <div className="hero-text flex w-1/2  text-center">
+        <div >
+          <h1 className="text-8xl font-bold text-[#254f1a] ">A link in bio built for you.</h1>
+        <p className=" text-xl text-left mr-5 max-w-lg">Join 70M+ people using Linktree for their link in bio. One link to help you share everything you create, curate and sell from your Instagram, TikTok, Twitter, YouTube and other social media profiles.</p></div>
+      
+      </div>
+      <div className="hero-slider flex bg-amber-400 w-1/2">jhkjl
 
+      </div>
+    </div>
     </section>
    </main>
   );
--- component/Navbar.js
+++ component/Navbar.js
@@ -2,16 +2,16 @@ import React from 'react'
 import Image from 'next/image'
 const Navbar = () => {
   return (
-  <nav className='bg-white rounded-full py-5 w-[80vw] absolute top-10 right-[10vw] flex justify-between '>
-    <div className='logo flex items-center mx-10 '> <Image alt='logo' src="https://cdn.prod.website-files.com/666255f7f2126f4e8cec6f8f/66634daccb34e6d65a41c76d_download.svg" width={150} height={150}></Image>
+  <nav className='bg-white rounded-full py-8 w-[80vw] absolute top-10 right-[10vw] flex justify-between '>
+    <div className='logo flex items-center mx-10 '> <Image alt='logo' src="https://cdn.prod.website-files.com/666255f7f2126f4e8cec6f8f/66634daccb34e6d65a41c76d_download.svg" width={135} height={135}></Image>
 
      <div className='flex items-center ml-10'>
          <ul className='flex gap-6 text-lg '>
-            <li>Product</li>
-            <li>Templates</li>
-            <li>Marketplace</li>
-            <li>Lern</li>
-            <li>Pricing</li>
+            <li className='text-xl'>Product</li>
+            <li className='text-xl'>Templates</li>
+            <li className='text-xl'>Marketplace</li>
+            <li className='text-xl'>Learn</li>
+            <li className='text-xl'>Pricing</li>
             
         </ul>
         </div>
