d971b1cee10434674baef22c4f15c5d89f2a8878
BalouchGITHUB <talha12346789khan@gmail.com>
2026-05-18 20:00:57 +0500

    18/05/2026 7

--- app/page.js
+++ app/page.js
@@ -16,7 +16,7 @@ export default function Home() {
 
   return (
    <main>
-    <section className="bg-[#d2e823] min-h-screen grid grid-cols-1 md:grid-cols-2 gap-8 md:gap-0  px-4 md:px-0">
+    <section className="bg-[#d2e823] min-h-full grid grid-cols-1 md:grid-cols-2 gap-8 md:gap-0  px-4 md:px-0">
    
     <div className="flex flex-col justify-center md:ml-[10vw] mt-50">
       <p className="text-cyan-800 text-4xl md:text-6xl lg:text-7xl font-bold leading-tight">A link in bio </p>
--- component/Footer.js
+++ component/Footer.js
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
    <footer className="text-center py-4">
-      <p>© 2026 Talha Khan. All rights reserved.</p>
+      <p>© 2026 Linktree-clone. All rights reserved.</p>
     </footer>
   )
 }
